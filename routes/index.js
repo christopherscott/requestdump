2a3fea07de916d5e2f828bbc2a2d282959dda4f1
Christopher Scott Hernandez <c.scott.hernandez@gmail.com>
2012-08-11 09:34:37 -0400

    updated title'

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ exports.index = function(req, res){
   res.render('index', {
     headers: req.headers,
     asset_prefix: process.env.ASSET_PREFIX || '',
-    title: 'Request Dump'
+    title: 'RequestDump'
   });
 };
